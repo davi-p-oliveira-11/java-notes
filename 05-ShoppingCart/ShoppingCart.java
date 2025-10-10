80dd6bdf233873112c64d3446efd6dd7928df8b4
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-10-10 14:48:06 -0300

    added shopping cart

---
+++ 05-ShoppingCart/ShoppingCart.java
@@ -0,0 +1,33 @@
+import  java.util.Scanner;
+
+public class ShoppingCart {
+    public static void main(String[] args) {
+
+        // SHopping cart program
+
+        Scanner scanner = new Scanner(System.in);
+
+        String item;
+        double price;
+        int quantity;
+        char currency = '$';
+        double total;
+
+        System.out.println("What item would you like to buy ?: ");
+        item = scanner.nextLine();
+
+        System.out.println("What is the price for each ?: ");
+        price = scanner.nextDouble();
+
+        System.out.println("How many would you like?: ");
+        quantity = scanner.nextInt();
+
+        total = price * quantity;
+
+        System.out.println("\n You have bought " + quantity + " " + item + "/s");
+        System.out.println("Your total is " + currency + total);
+
+        scanner.close();
+
+    }
+}
