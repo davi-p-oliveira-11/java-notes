f9a213bbe3d6d6b7ce0430422ca81b2d03f0f4d3
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-10-15 16:55:44 -0300

    added overloaded methods

---
+++ 26-OverloadedMethods/OverloadedMethods.java
@@ -0,0 +1,15 @@
+public class OverloadedMethods {
+    public static void main(String[] args) {
+
+        // overloaded methods = methods that share the same name,
+        //                      but different parameters
+        //                      signature = name + parameters
+        System.out.println(add(1, 2, 3));
+    }
+    static double add(double a, double b) {
+        return a + b;
+    }
+    static double add(double a, double b, double c) {
+        return a + b + c;
+    }
+}
