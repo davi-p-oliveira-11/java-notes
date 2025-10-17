e5a89bde61ef39812a955b14eacdbfcc808c5c7f
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-10-17 11:46:42 -0300

    added superkeyword

---
+++ 43-Inheritance/Inheritance.java
@@ -0,0 +1,16 @@
+public class Inheritance {
+    public static void main(String[] args) {
+
+        // Inheritance = One class inherits the attributes and methods
+        //                         from another class.
+        //                         Child <- Parent <- Grandparent
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Plant plant = new Plant();
+
+    }
+}
+
+
+
