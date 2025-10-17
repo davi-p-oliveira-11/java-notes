66436d7e96de34db14b95a2a6828f6a2ec2360fd
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-10-17 13:08:00 -0300

    arraylist added

---
+++ 55-ArrayList/ArrayList2.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayList2 {
+    public static void main(String[] args) {
+
+        // ArrayList = A resizeable array that stores objects (autoboxing)
+        //                     Arrays are fixed in size, but ArrayLists can change
+
+        ArrayList<String> fruits = new ArrayList<>();
+
+        fruits.add("Apple");
+        fruits.add("Orange");
+        fruits.add("Banana");
+        fruits.add("Coconut");
+
+        //fruits.remove(0);
+        //fruits.set(0, "Pineapple");
+
+        Collections.sort(fruits);
+
+        for(String fruit : fruits){
+            System.out.println(fruit);
+        }
+    }
+}
