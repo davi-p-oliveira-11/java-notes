99a7c288f020c81089a16ab4efdbc9fb1d3ee181
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-10-17 13:46:01 -0300

    added AnonymousClasses

---
+++ 62-AnonymousClasses/AnonymousClasses.java
@@ -0,0 +1,19 @@
+public class AnonymousClasses {
+    public static void main(String[] args) {
+
+        // Anonymous class = A class that doesn't have a name. Cannot be reused.
+        //                                     Add custom behavior without having to create a new class.
+        //                                     Often used for one time uses (TimerTask, Runnable, callbacks)
+
+        Dog5 dog1 = new Dog5();
+        Dog5 dog2 = new Dog5(){
+            @Override
+            void speak(){
+                System.out.println("Scooby Doo says *Ruh Roh*");
+            }
+        };
+
+        dog1.speak();
+        dog2.speak();
+    }
+}
---
+++ 62-AnonymousClasses/Dog5.java
@@ -0,0 +1,7 @@
+
+public class Dog5 {
+
+    void speak(){
+        System.out.println("The dog goes *woof*");
+    }
+}
