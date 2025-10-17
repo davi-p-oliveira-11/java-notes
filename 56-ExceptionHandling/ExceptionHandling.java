2c3871331b10aeadf944674b624eb4222714636e
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-10-17 13:15:19 -0300

    added exceptionhandling

--- 56-ExceptionHandling/ExceptionHandling.java
+++ 56-ExceptionHandling/ExceptionHandling.java
@@ -1,26 +1,31 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class ExceptionHandling {
     public static void main(String[] args) {
 
-        // ArrayList = A resizeable array that stores objects (autoboxing)
-        //                     Arrays are fixed in size, but ArrayLists can change
+        // Exception = An event that interrupts the normal flow of a program
+        //             (Dividing by zero, file not found, mismatch input type)
+        //             Surround any dangerous code with a try{} block
+        //             try{}, catch{}, finally{}
 
-        ArrayList<String> fruits = new ArrayList<>();
-
-        fruits.add("Apple");
-        fruits.add("Orange");
-        fruits.add("Banana");
-        fruits.add("Coconut");
-
-        //fruits.remove(0);
-        //fruits.set(0, "Pineapple");
-
-        Collections.sort(fruits);
-
-        for(String fruit : fruits){
-            System.out.println(fruit);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter a number: ");
+            int number = scanner.nextInt();
+            System.out.println(number);
+        }
+        catch (InputMismatchException e) {
+            System.out.println("That wasn't a number!");
+        }
+        catch (ArithmeticException e) {
+            System.out.println("YOU CAN'T DIVIDE BY ZERO!");
+        }
+        catch (Exception e) {
+            // SAFETY NET
+            System.out.println("Something went wrong");
+        }
+        finally {
+            System.out.println("This always executes");
         }
     }
 }
