c205d45c2367d3c908245034aa791006276c8885
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-10-20 16:48:24 -0300

    adjusted AnonymousClasses

--- 62-AnonymousClasses/AnonymousClasses.java
+++ 62-AnonymousClasses/AnonymousClasses.java
@@ -2,8 +2,8 @@ public class AnonymousClasses {
     public static void main(String[] args) {
 
         // Anonymous class = A class that doesn't have a name. Cannot be reused.
-        //                                     Add custom behavior without having to create a new class.
-        //                                     Often used for one time uses (TimerTask, Runnable, callbacks)
+        //                   Add custom behavior without having to create a new class.
+        //                   Often used for one time uses (TimerTask, Runnable, callbacks)
 
         Dog5 dog1 = new Dog5();
         Dog5 dog2 = new Dog5(){
